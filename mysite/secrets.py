b4cadd5fd1551bae3f1fd05e61b508ff45e7d432
Hope Tambala <hopetambala@outlook.com>
2018-12-06 00:17:41 -0500

    remove secrets

--- mysite/secrets.py
+++
@@ -1,6 +0,0 @@
-DATABASES_PASSWORD = 'animal$$911'
-
-SECURITEE_KEY = 'v2gko*vvk_dfk)^wff!@^%hl=5_k5_2u9n+q+tg&l2-3edkde-'
-
-SOCIAL_AUTH_KEEEE = '166401922081-a0jgot6756sn82v3rvg39qogjgg2s7r1.apps.googleusercontent.com'
-SOCIAL_AUTH_SECRET = 'xwZlfhj1FAxhmHN-DCyYH0Gb'
