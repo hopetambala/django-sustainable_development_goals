56aa29877f60cba945641723077995667086691b
Hope Tambala <hopetambala@outlook.com>
2018-12-16 07:49:48 -0800

    fix(): change database

--- mysite/settings.py
+++ mysite/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'unesco_heritage_sites',
+        'NAME': 'unsdg',
         'USER': 'root',
         'PASSWORD': DATABASES_PASSWORD,
         'OPTIONS': {
