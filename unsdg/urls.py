03c51d36868d7b49ae1c2a1c491fbf58d4f52d13
Hope Tambala <hopetambala@outlook.com>
2018-12-17 22:22:51 -0800

    create(): routes

--- unsdg/urls.py
+++ unsdg/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
    path('', views.HomePageView.as_view(), name='home'),
    path('about/', views.AboutPageView.as_view(), name='about'),
+   path('indicator_value_names/', views.IndicatorNameListView.as_view(), name='indicator_names'),
+   path('goals/', views.GoalListView.as_view(), name='goals'),
+   path('targets/', views.TargetListView.as_view(), name='targets'),
 ]
