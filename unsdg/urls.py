b4621923d616cf8fd9eb433ca669769bbdd1d39c
Hope Tambala <hopetambala@outlook.com>
2018-12-18 00:01:00 -0800

    Update urls.py

--- unsdg/urls.py
+++ unsdg/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
    path('', views.HomePageView.as_view(), name='home'),
+   path('all/', views.CountryTargetIndicatorListView.as_view(), name='country_target_indicators'),
    path('about/', views.AboutPageView.as_view(), name='about'),
    path('indicator_value_names/', views.IndicatorNameListView.as_view(), name='indicator_names'),
    path('goals/', views.GoalListView.as_view(), name='goals'),
