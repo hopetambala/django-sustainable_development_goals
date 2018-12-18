17f004a0cc4f013dcc913ce874468b1f887ad864
Hope Tambala <hopetambala@outlook.com>
2018-12-18 00:00:21 -0800

    chore(): social login routes

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,11 +2,16 @@ from django.conf import settings
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponseRedirect
 from django.urls import path, include
+
 
 urlpatterns = [
     path('', lambda r: HttpResponseRedirect('unsdg/')),
     path('admin/', admin.site.urls),
     path('unsdg/', include('unsdg.urls')),
+    path('auth/', include('social_django.urls', namespace='social')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
 ]
