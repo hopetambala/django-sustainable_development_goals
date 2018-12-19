b143941da9750e236dee3b92b46c1af534dd785e
Hope Tambala <hopetambala@outlook.com>
2018-12-19 07:09:53 -0800

    fix(): add filter route

--- unsdg/urls.py
+++ unsdg/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
    path('cti/new/', views.CTIndicatorCreateView.as_view(), name='country_target_indicator_detail_new'),
    path('cti/<int:pk>/delete/', views.CTIndicatorDeleteView.as_view(), name='country_target_indicator_detail_delete'),
    path('cti/<int:pk>/update/', views.CTIndicatorUpdateView.as_view(), name='country_target_indicator_detail_update'),
+   path('cti/filter/', views.CTIndicatorFilterView.as_view(), name='country_target_indicator_filter'),
    path('about/', views.AboutPageView.as_view(), name='about'),
    path('indicator_value_names/', views.IndicatorNameListView.as_view(), name='indicator_names'),
    path('goals/', views.GoalListView.as_view(), name='goals'),
